3dd8ec9798455ec58af6d5b336d07d4445b3123a
RobertCSternberg <136265104+RobertCSternberg@users.noreply.github.com>
2023-06-26 09:57:13 -0500

    Update run_scan.sh
    Add push uptime monitor capability and add timestamp in central time to the top of the Arp Scan Results Page.

--- run_scan.sh
+++ run_scan.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 while true
 do
-  echo "<html><head><title>Arp Scan Results</title></head><body><h1>Arp Scan Results</h1><pre>" > /tmp/index.html
+  echo "<html><head><title>Arp Scan Results</title></head><body><h1>Arp Scan Results</h1>" > /tmp/index.html
+  echo "<p>Last updated: $(TZ='America/Chicago' date '+%B %d, %Y %I:%M:%S %p') Central Time US</p>" >> /tmp/index.html
+  echo "<pre>" >> /tmp/index.html
+
+  # For uptime monitoring uncomment the line and replace the URL with the one you want to call from your uptime push monitor of choice, like Uptime Kuma.  
+  # /usr/bin/curl "https://google.com" > /dev/null 2>&1
+
+  # Replace the arp-scan address with your network address and subnet mask.  Most commonly 192.168.1.0/24
   arp-scan 192.168.1.0/24 >> /tmp/index.html 2>> /tmp/arp-scan-errors.log
   if [ $? -ne 0 ]; then
     echo "Arp-scan failed. See /tmp/arp-scan-errors.log for details." >> /tmp/index.html
@@ -9,5 +16,5 @@ do
   echo "</pre></body></html>" >> /tmp/index.html
   mv /tmp/index.html /usr/share/nginx/html/index.html
   cp /usr/share/nginx/html/index.html /usr/share/nginx/html/index-copy.html
-  sleep 60
+  sleep 30
 done
