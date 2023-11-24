eeb7c4d20c45d02df4be006de1451e55c3c388ce
Alexander Stumpf <github@TheStumpfs.de>
2023-11-24 23:46:56 +0100

    Add json option

--- scan.sh
+++ scan.sh
@@ -1,25 +1,50 @@
 #!/bin/sh
-echo "<html><head><title>Arp Scan Results</title></head><body><h1>Arp Scan Results</h1>"
-echo "<p>Last updated: $(date '+%d.%B %Y %H:%M:%S %Z')</p>"
-echo "<pre>"
 
+JSON=false
 BASECOMMAND="arp-scan"
-if [ "$1" = "--parsable" ]; then
-  BASECOMMAND="arp-scan -x -D -F ip:\${IP};mac:\${mac};rtt:\${rtt};vendor:\${vendor}"
+DATE="$(date '+%d.%B %Y %H:%M:%S %Z')"
+
+if [ "$1" = "--json" ]; then
+  JSON=true
+  BASECOMMAND="arp-scan -x -D -F \{\"ip\":\"\${IP}\",\"mac\":\"\${mac}\",\"rtt\":\"\${rtt}\",\"vendor\":\"\${vendor}\"\}"
   shift
 fi
 
+if [ $JSON = 'true' ]; then
+  echo -n "{\"date\":\"${DATE}\",\"results\":["
+else
+  echo "<html><head><title>Arp Scan Results</title></head><body><h1>Arp Scan Results</h1>"
+  echo "<p>Last updated: ${DATE}</p>"
+  echo "<pre>"
+fi
+
+INLOOP=false
 while [ "$1" != "" ]
 do
+  if [ $INLOOP = 'true' ]; then
+    echo -n ','
+  else
+    INLOOP=true
+  fi
+
   COMMAND="$BASECOMMAND $1"
   if [ "$2" != "" ]; then
     COMMAND="$COMMAND -I $2"
     shift
   fi
   shift
-  $COMMAND
+  if [ $JSON  = 'true' ]; then
+    $COMMAND | paste -d, -s -
+  else
+    $COMMAND
+    echo
+  fi
   if [ $? -ne 0 ]; then
-    echo "Arp-scan failed. See log for details."
+    echo '"Arp-scan failed. See log for details."'
   fi
 done
-echo "</pre></body></html>"
+if [ $JSON  = 'true' ]; then
+  echo ']}'
+else
+  echo "</pre></body></html>"
+fi
