ba28fb2eef9de81fb9fcacb78204fdfbce59672c
Alexander Stumpf <github@TheStumpfs.de>
2023-11-24 23:46:40 +0100

    Use temporary file to prevent partial results

--- entry.sh
+++ entry.sh
@@ -4,8 +4,19 @@ echo "Received following parameters for arp-scanning: $@"
 echo Starting nginx
 nginx
 echo Starting arp-scan loop
+
+TIME=60
+if [ "$1" = "--time" ]; then
+  TIME=$2
+  shift
+  shift
+fi
+
 while true
 do
-    ./scan.sh "$@" > /usr/share/nginx/html/index.html
-    sleep 60
+    TEMPFILE=$(mktemp -p /usr/share/nginx/html index.XXXXXX)
+    ./scan.sh "$@" > $TEMPFILE
+    chmod a+r $TEMPFILE
+    mv $TEMPFILE /usr/share/nginx/html/index.html
+    sleep $TIME
 done
